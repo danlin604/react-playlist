a691abc27fb2e635de049ba1d8f1a4b807ca99d6
Daniel Lin <danlin604@gmail.com>
2016-12-25 14:14:10 -0800

    Tutorial 5 complete
    Signed-off-by: Daniel Lin <danlin604@gmail.com>

--- src/app/index.js
+++ src/app/index.js
@@ -3,18 +3,25 @@ var ReactDOM = require('react-dom');
 
 //Create component
 var TodoComponent = React.createClass({
+    getInitialState: function() {
+       return {
+           todos: ['washup', 'eat some cheese', 'nap']
+       } 
+    }, // getInitialState
     render: function() {
         return(
-            <div>
-                <p><strong>Cheese name:</strong> {this.props.cheese.name}</p>
-                <p><strong>Cheese smell factor:</strong> {this.props.cheese.smellFactor}</p>
-                <p><strong>Cheese price:</strong> {this.props.cheese.price}</p>
+            <div id="todo-list">
+                <p>The busiest people have the most leisure...</p>
+                <ul>
+                    <li>{this.state.todos[0]}</li>
+                    <li>{this.state.todos[1]}</li>
+                    <li>{this.state.todos[2]}</li>
+                </ul>
+                <ListComponent todos={this.state.todos} />
             </div>
         );
-    }
+    } // render
 });
 
-var myCheese = {name: 'Camembert', smellFactor: 'Extreme pong', price: '3.50'};
-
 //put component into html page
-ReactDOM.render(<TodoComponent mssg="I like cheese" cheese={myCheese} />, document.getElementById('todo-wrapper'));
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
